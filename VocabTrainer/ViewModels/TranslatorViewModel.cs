19c10441b53d3960b8b610347de1161853aacfc5
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-10-04 20:11:35 +0200

    #init TranslatorViewModel

---
+++ VocabTrainer/ViewModels/TranslatorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VocabTrainer.Views;
+
+namespace VocabTrainer.ViewModels {
+    internal class TranslatorViewModel {
+
+        public MainWindow ParentWindow { get; }
+        public TranslatorViewModel(MainWindow parentWindow) {
+            ParentWindow = parentWindow;
+            ParentWindow.DataContext = new TranslatorView();
+        }
+    }
+}
