75c473d874335e3536c54ee46bdc0f3edf449861
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-10-29 21:47:33 +0100

    #add and designed view for entries with true/false

---
+++ VocabTrainer/ViewModels/SettingsEntryTrueFalseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocabTrainer.ViewModels {
+    internal class SettingsEntryTrueFalseViewModel : BaseViewModel {
+        private string _condition;
+        public string Condition {
+            get => _condition;
+            set {
+                _condition = value;
+                OnPropertyChanged(nameof(Condition));
+            }
+        }
+        private bool _isTrue;
+        public bool IsTrue {
+            get => _isTrue;
+            set {
+                _isTrue = value;
+                OnPropertyChanged(nameof(IsTrue));
+            }
+        }
+        public SettingsEntryTrueFalseViewModel(string condition, bool isTrue) {
+            Condition = condition;
+            IsTrue = isTrue;
+        }
+    }
+}
