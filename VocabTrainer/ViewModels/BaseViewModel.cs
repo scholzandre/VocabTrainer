b54de607abbeaabf00002df3bf951da4361af3e9
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-10-29 21:38:23 +0100

    #add BaseViewModel with implemented INotifyPropertyChanged

---
+++ VocabTrainer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocabTrainer.ViewModels {
+    public abstract class BaseViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
