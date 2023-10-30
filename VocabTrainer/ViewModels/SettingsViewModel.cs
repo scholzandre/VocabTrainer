47497650de37d1c18398c4b606f15823beec7874
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-10-30 14:32:37 +0100

    #update SettingsViewModel
    - update view with FillCollection method
    - List with views for settings with checkbox

--- VocabTrainer/ViewModels/SettingsViewModel.cs
+++ VocabTrainer/ViewModels/SettingsViewModel.cs
@@ -1,52 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Navigation;
 using VocabTrainer.Views;
 
 namespace VocabTrainer.ViewModels {
-    public class SettingsViewModel {
+    public class SettingsViewModel : BaseViewModel {
         private readonly List<Settings> _settings;
-        public ObservableCollection<UserControl> ListEntries { get; set; }
+        private readonly List<WordlistsList> _wordlists;
+        public ObservableCollection<UserControl> _listGeneralSettings;
+        public ObservableCollection<UserControl> ListGeneralSettings {
+            get => _listGeneralSettings;
+            set {
+                _listGeneralSettings = value;
+                OnPropertyChanged(nameof(ListGeneralSettings));
+            }
+        }
+        public ObservableCollection<UserControl> _listWordlists;
+        public ObservableCollection<UserControl> ListWordlists {
+            get => _listWordlists;
+            set {
+                _listWordlists = value;
+                OnPropertyChanged(nameof(ListWordlists));
+            }
+        }
         private MainViewModel _parent;
         public SettingsViewModel(MainViewModel parent) {
             _parent = parent;
             _settings = _parent.SettingsList;
-            ListEntries = new ObservableCollection<UserControl>();
+            _wordlists = _parent.ListWordlist;
             FillCollection();
         }
 
-        private void FillCollection() {
-            foreach (Settings setting in _settings) {
-                if (setting.LearningMode > 0 ||
-                    setting.Condition == "random order" ||
-                    setting.Condition == "list order") {
+        public void FillCollection() {
+            ListGeneralSettings = new ObservableCollection<UserControl>();
+            ListWordlists = new ObservableCollection<UserControl>();
+            for (int i = 0; i < _settings.Count; i++) {
+                if (_settings[i].LearningMode > 0 ||
+                    _settings[i].Condition == "random order" ||
+                    _settings[i].Condition == "list order") {
                     Type viewType = typeof(SettingsEntryTrueFalseView);
                     UserControl tempControl = (UserControl)Activator.CreateInstance(viewType);
-                    tempControl.DataContext = new SettingsEntryTrueFalseViewModel(setting.Condition, setting.IsTrue);
-                    ListEntries.Add(tempControl);
+                    tempControl.DataContext = new SettingsEntryTrueFalseViewModel(_settings[i].Condition, _settings[i].IsTrue, _settings, this, _wordlists);
+                    ListGeneralSettings.Add(tempControl);
                 } else {
                     Type viewType = typeof(SettingsEntryTextView);
                     UserControl tempControl = (UserControl)Activator.CreateInstance(viewType);
-                    if (setting.APIKey != null) { 
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.APIKey, _settings, _parent);
-                    } else if (setting.BorderBackground != null) {
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.BorderBackground, _settings, _parent);
-                    } else if (setting.BorderBrush != null) {
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.BorderBrush, _settings, _parent);
-                    } else if (setting.ButtonsBackground != null) {
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.ButtonsBackground, _settings, _parent);
-                    } else if (setting.ButtonsForeground != null) {
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.ButtonsForeground, _settings, _parent);
-                    } else if (setting.NavBarBackground != null) {
-                        tempControl.DataContext = new SettingsEntryTextViewModel(setting.Condition, setting.NavBarBackground, _settings, _parent);
+                    if (_settings[i].APIKey != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].APIKey, _settings, _parent);
+                    } else if (_settings[i].BorderBackground != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].BorderBackground, _settings, _parent);
+                    } else if (_settings[i].BorderBrush != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].BorderBrush, _settings, _parent);
+                    } else if (_settings[i].ButtonsBackground != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].ButtonsBackground, _settings, _parent);
+                    } else if (_settings[i].ButtonsForeground != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].ButtonsForeground, _settings, _parent);
+                    } else if (_settings[i].NavBarBackground != null) {
+                        tempControl.DataContext = new SettingsEntryTextViewModel(_settings[i].Condition, _settings[i].NavBarBackground, _settings, _parent);
                     }
-                    ListEntries.Add(tempControl);
+                    ListGeneralSettings.Add(tempControl);
                 }
+            }
+            for (int i = 0; i < _wordlists.Count; i++) {
+                Type viewType = typeof(SettingsEntryTrueFalseView);
+                UserControl tempControl = (UserControl)Activator.CreateInstance(viewType);
+                tempControl.DataContext = new SettingsEntryTrueFalseViewModel(_wordlists[i].WordlistName, _wordlists[i].IsTrue, _settings, this, _wordlists);
+                ListWordlists.Add(tempControl);
             }
         }
     }
