bd11bdb433589c947b9beae01d4dc8f319c72fa2
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-11-05 21:23:53 +0100

    #removed code behind

--- VocabTrainer/Views/AddView.xaml.cs
+++ VocabTrainer/Views/AddView.xaml.cs
@@ -1,46 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace VocabTrainer.Views {
     public partial class AddView : UserControl {
         public AddView() {
             InitializeComponent();
-            FillComboBox();
-        }
-        public void FillComboBox() {
-            List<WordlistsList> wordlists = WordlistsList.GetWordlistsList();
-            for (int i = 0; i < wordlists.Count; i++)
-                if (wordlists[i].WordlistName != "Marked" &&
-                    wordlists[i].WordlistName != "Seen" &&
-                    wordlists[i].WordlistName != "NotSeen" &&
-                    wordlists[i].WordlistName != "LastTimeWrong") { 
-                    comboWordlists.Items.Add($"{wordlists[i].WordlistName} ({wordlists[i].FirstLanguage}, {wordlists[i].SecondLanguage})");
-                }
-        }
-        public void AddWord(object sender, RoutedEventArgs e) {
-            if (comboWordlists.Text != "") {
-                (bool isTrue, string returnText) = VocabularyEntry.CheckInput(comboWordlists.Text, germanWord.Text, englishWord.Text);
-                if (isTrue) {
-                    addingSuccessful.Text = $"{germanWord.Text} ({firstLanguage.Text}) and {englishWord.Text} ({secondLanguage.Text}) have been successfully added";
-                    germanWord.Text = String.Empty;
-                    englishWord.Text = String.Empty;
-                } else addingSuccessful.Text = returnText;
-            }
         }
         private void Windows_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Windows_MouseDown(sender, e);
-        }
-        private void Closed(object sender, EventArgs e) {
-            if (comboWordlists.Text != "") {
-                int indexKomma = comboWordlists.Text.IndexOf(',');
-                int indexBracketsOpen = comboWordlists.Text.IndexOf('(');
-                int indexBracketsClose = comboWordlists.Text.IndexOf(')');
-                firstLanguage.Text = comboWordlists.Text.Substring(indexBracketsOpen + 1, indexKomma - 1 - indexBracketsOpen).Trim();
-                secondLanguage.Text = comboWordlists.Text.Substring(indexKomma + 1, indexBracketsClose - 1 - indexKomma).Trim();
-            }
         }
     }
 }
