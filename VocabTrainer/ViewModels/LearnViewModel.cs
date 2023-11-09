7762e2fd47a96f555e6681195edb9377b61c8270
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-11-09 21:43:18 +0100

    #update list based on order

--- VocabTrainer/ViewModels/LearnViewModel.cs
+++ VocabTrainer/ViewModels/LearnViewModel.cs
@@ -28,32 +28,97 @@ namespace VocabTrainer.ViewModels
         private List<Settings> _settings;
         private List<Settings> _learningModes = new List<Settings>();
         private List<WordlistsList> _wordlists = new List<WordlistsList>();
-        private List<VocabularyEntry> entries = new List<VocabularyEntry>();
+        private List<VocabularyEntry> _entries = new List<VocabularyEntry>();
+        public List<VocabularyEntry> Entries { 
+            get => _entries;
+            set { 
+                _entries = value;
+                OnPropertyChanged(nameof(Entries));
+            }
+        }
+        public int Counter { get; set; } = 0;
         public LearnViewModel() {
             GetSettings();
+            GetEntries();
+            ShowLearnMode();
+        }
+
+        public void ShowLearnMode() {
             Type viewType = typeof(LearningModeOneView);
             UserControl = (UserControl)Activator.CreateInstance(viewType);
-            UserControl.DataContext = new LearningModeOneViewModel();
+            UserControl.DataContext = new LearningModeOneViewModel(this);
+            if (Counter < Entries.Count) {
+                Counter++;
+            } else {
+                Counter = 0;
+                GetEntries();
+            }
         }
-        private void GetSettings() { 
+
+        private void GetSettings() {
             _settings = Settings.GetSettings();
-            List<WordlistsList> tempWordlist = WordlistsList.GetWordlistsList();
-            for(int i = 2; i < 6; i++) {
+            for (int i = 2; i < 6; i++) {
                 if (_settings[i].LearningMode > 0) {
                     _learningModes.Add(_settings[i]);
                 }
             }
-            for (int i = 0; i < tempWordlist.Count; i++) { 
-                if (tempWordlist[i].IsTrue) _wordlists.Add(tempWordlist[i]);
+        }
+
+        private void GetEntries() {
+            List<WordlistsList> tempWordlist = WordlistsList.GetWordlistsList();
+            List<VocabularyEntry> tempEntries = new List<VocabularyEntry>();
+            Random random = new Random();
+            for (int i = 0; i < tempWordlist.Count; i++) {
+                if (tempWordlist[i].IsTrue) {
+                    _wordlists.Add(tempWordlist[i]);
+                    tempEntries.AddRange(
+                        VocabularyEntry.GetData(
+                            new VocabularyEntry() {
+                                FilePath = $"{VocabularyEntry.FirstPartFilePath}{tempWordlist[i].WordlistName}_{tempWordlist[i].FirstLanguage}_{tempWordlist[i].SecondLanguage}{VocabularyEntry.SecondPartFilePath}"
+                            }
+                        )
+                    );
+                }
             }
-            for (int i = 0; i < _wordlists.Count; i++) {
-                entries.AddRange(
-                    VocabularyEntry.GetData(
-                        new VocabularyEntry() { 
-                            FilePath = $"{VocabularyEntry.FirstPartFilePath}{_wordlists[i].WordlistName}_{_wordlists[i].FirstLanguage}_{_wordlists[i].SecondLanguage}{VocabularyEntry.SecondPartFilePath}"
-                        }
-                    )
-                );
+            if (_settings[0].IsTrue) {
+                while (tempEntries.Count > 0) { 
+                    int index = random.Next(tempEntries.Count);
+                    Entries.Add(tempEntries[index]);
+                    tempEntries.Remove(tempEntries[index]);
+                }
+            } else {
+                List<VocabularyEntry> seen = new List<VocabularyEntry>();
+                List<VocabularyEntry> notSeen = new List<VocabularyEntry>();
+                List<VocabularyEntry> known = new List<VocabularyEntry>();
+                List<VocabularyEntry> lastTimeWrong = new List<VocabularyEntry>();
+                int counterSeen = 0;
+                int counterNotSeen = 0;
+                int counterKnown = 0;
+                int counterLastTimeWrong = 0;
+                foreach(VocabularyEntry tempEntry in tempEntries) { 
+                    if (tempEntry.LastTimeWrong) lastTimeWrong.Add(tempEntry);
+                    else if (!tempEntry.Seen) notSeen.Add(tempEntry);
+                    else if (tempEntry.Seen) seen.Add(tempEntry);
+                    else if (tempEntry.Repeated > 5) known.Add(tempEntry);
+                }
+                for (int i = 0; i < tempEntries.Count; i++) {
+                    int percentage = random.Next(1, 101);
+                    if (percentage > 60 && counterLastTimeWrong < lastTimeWrong.Count) {
+                        Entries.Add(lastTimeWrong[counterLastTimeWrong]);
+                        counterLastTimeWrong++;
+                    } else if (percentage > 30 && percentage <= 60 && counterNotSeen < notSeen.Count) {
+                        Entries.Add(notSeen[counterNotSeen]);
+                        counterNotSeen++;
+                    } else if (percentage > 10 && percentage <= 30 && counterSeen < seen.Count) {
+                        Entries.Add(seen[counterSeen]);
+                        counterSeen++;
+                    } else if (percentage > 0 && percentage <= 10 && counterKnown < known.Count) {
+                        Entries.Add(known[counterKnown]);
+                        counterKnown++;
+                    } else {
+                        i--;
+                    }
+                }
             }
         }
     }
