eb3da6950acc4886b314d64e8a0c66fb139ee9dd
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-11-10 10:38:46 +0100

    #update LearningModeOne with Progress and marking ability

--- VocabTrainer/ViewModels/LearningModeOneViewModel.cs
+++ VocabTrainer/ViewModels/LearningModeOneViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,14 +43,28 @@ namespace VocabTrainer.ViewModels {
                 OnPropertyChanged(nameof(SecondLanguage));
             }
         }
+        private string _star;
+        public string Star {
+            get => _star;
+            set {
+                _star = value;
+                OnPropertyChanged(nameof(Star));
+            }
+        }
         public bool IsMarked { get; set; }
-        LearnViewModel _parent;
+        private readonly LearnViewModel _parent;
+        static VocabularyEntry markEntry = new VocabularyEntry() { FilePath = $"{VocabularyEntry.FirstPartFilePath}Marked{VocabularyEntry.SecondPartFilePath}" };
+        private List<VocabularyEntry> markedEntries = VocabularyEntry.GetData(markEntry);
+        private int _counter;
         public LearningModeOneViewModel(LearnViewModel parent) { 
             _parent = parent;
-            FirstWord = _parent.Entries[_parent.Counter].German;
-            SecondWord = _parent.Entries[_parent.Counter].English;
-            FirstLanguage = _parent.Entries[_parent.Counter].FirstLanguage;
-            SecondLanguage = _parent.Entries[_parent.Counter].SecondLanguage;
+            _counter = _parent.Counter;
+            FirstWord = _parent.Entries[_counter].German;
+            SecondWord = _parent.Entries[_counter].English;
+            FirstLanguage = _parent.Entries[_counter].FirstLanguage;
+            SecondLanguage = _parent.Entries[_counter].SecondLanguage;
+
+            Star = (markedEntries.Contains(_parent.Entries[_counter])) ? "★" : "☆";
         }
 
         private bool CanExecuteCommand(object arg) {
@@ -57,7 +72,25 @@ namespace VocabTrainer.ViewModels {
         }
         public ICommand NextWordCommand => new RelayCommand(NextWord, CanExecuteCommand);
         private void NextWord(object obj) {
+            _parent.Entries[_counter].Seen = true;
+            _parent.Entries[_counter].Repeated += 1;
+            VocabularyEntry tempEntry = new VocabularyEntry() {
+                FilePath = $"{VocabularyEntry.FirstPartFilePath}{_parent.Entries[_counter].WordList}{VocabularyEntry.SecondPartFilePath}"
+            };
+            VocabularyEntry.WriteData(tempEntry, _parent.Entries);
             _parent.ShowLearnMode();
+        }
+
+        public ICommand MarkEntryCommand => new RelayCommand(MarkEntry, CanExecuteCommand);
+        private void MarkEntry(object obj) {
+            if (Star == "☆") {
+                Star = "★";
+                markedEntries.Add(_parent.Entries[_counter]);
+            } else {
+                Star = "☆";
+                markedEntries.Remove(_parent.Entries[_counter]);
+            }
+            VocabularyEntry.WriteData(markEntry, markedEntries);
         }
     }
 }
