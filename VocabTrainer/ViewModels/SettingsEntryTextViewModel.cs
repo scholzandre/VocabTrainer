4546a1be3d5d954a760284810b5a20fb0c87a924
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-11-16 22:01:15 +0100

    #removed not used directories

--- VocabTrainer/ViewModels/SettingsEntryTextViewModel.cs
+++ VocabTrainer/ViewModels/SettingsEntryTextViewModel.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Collections.Generic;
 using System.Windows.Input;
 using VocabTrainer.Models;
-using VocabTrainer.Views;
 
 namespace VocabTrainer.ViewModels {
     internal class SettingsEntryTextViewModel : BaseViewModel {
