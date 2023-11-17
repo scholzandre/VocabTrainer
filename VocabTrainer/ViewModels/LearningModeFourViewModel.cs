4d0132c5a9adf0f073dc90e22f88f014cd2ecee6
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-11-17 09:18:19 +0100

    #update Commands

--- VocabTrainer/ViewModels/LearningModeFourViewModel.cs
+++ VocabTrainer/ViewModels/LearningModeFourViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using VocabTrainer.Models;
+using VocabTrainer.Views;
 
 namespace VocabTrainer.ViewModels {
     public class LearningModeFourViewModel : BaseViewModel {
@@ -89,6 +93,51 @@ namespace VocabTrainer.ViewModels {
         private LearnViewModel _parent;
         public LearningModeFourViewModel(LearnViewModel parent) {
             _parent = parent;
+        }
+
+        private bool CanExecuteCommand(object arg) {
+            return true;
+        }
+        public ICommand SetFirstQuestionCommand => new RelayCommand(SetFirstQuestion, CanExecuteCommand);
+        private void SetFirstQuestion(object obj) {
+            
+        }
+        public ICommand SetSecondQuestionCommand => new RelayCommand(SetSecondQuestion, CanExecuteCommand);
+        private void SetSecondQuestion(object obj) {
+
+        }
+        public ICommand SetThirdQuestionCommand => new RelayCommand(SetThirdQuestion, CanExecuteCommand);
+        private void SetThirdQuestion(object obj) {
+
+        }
+        public ICommand SetFourthQuestionCommand => new RelayCommand(SetFourthQuestion, CanExecuteCommand);
+        private void SetFourthQuestion(object obj) {
+
+        }
+        public ICommand SetFifthQuestionCommand => new RelayCommand(SetFifthQuestion, CanExecuteCommand);
+        private void SetFifthQuestion(object obj) {
+
+        }
+
+        public ICommand SetFirstAnswerCommand => new RelayCommand(SetÁnswerQuestion, CanExecuteCommand);
+        private void SetÁnswerQuestion(object obj) {
+
+        }
+        public ICommand SetSecondAnswerCommand => new RelayCommand(SetSecondAnswer, CanExecuteCommand);
+        private void SetSecondAnswer(object obj) {
+
+        }
+        public ICommand SetThirdAnswerCommand => new RelayCommand(SetThirdAnswer, CanExecuteCommand);
+        private void SetThirdAnswer(object obj) {
+
+        }
+        public ICommand SetFourthAnswerCommand => new RelayCommand(SetFourthAnswer, CanExecuteCommand);
+        private void SetFourthAnswer(object obj) {
+
+        }
+        public ICommand SetFifthAnswerCommand => new RelayCommand(SetFifthAnswer, CanExecuteCommand);
+        private void SetFifthAnswer(object obj) {
+
         }
     }
 }
