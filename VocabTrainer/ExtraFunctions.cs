9a6d52e05a65c4eefd4841385c889f84ee0a507c
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-12-13 23:31:11 +0100

    #removed classes

--- VocabTrainer/ExtraFunctions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace VocabTrainer {
-    public class ExtraFunctions {
-        public static async Task Wait(int time = 1000) {
-            await Task.Delay(time);
-        }
-    }
-}
