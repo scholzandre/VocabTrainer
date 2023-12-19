b00ce2044710d87e267c2a076c6a7884a7f7b530
scholzandre <143190997+scholzandre@users.noreply.github.com>
2023-12-19 21:17:08 +0100

    #update VocabularyEntry
    - removed not used methods
    - fixed conditions

--- VocabTrainer/Models/VocabularyEntry.cs
+++ VocabTrainer/Models/VocabularyEntry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System;
 
 namespace VocabTrainer.Views {
     public class VocabularyEntry {
@@ -31,15 +32,11 @@ namespace VocabTrainer.Views {
         }
         public static void WriteData(VocabularyEntry entry, List<VocabularyEntry> vocabulary) {
             for (int i = 0; i < vocabulary.Count; i++) {
-                int index = entry.FilePath.LastIndexOf('\\');
-                string tempWordlist = entry.FilePath.Substring(index + 1, entry.FilePath.Count() - (index + 6)); 
-                string languages = tempWordlist.Substring(tempWordlist.IndexOf('_') + 1);
-
-                if (languages != "Marked" &&
-                    languages != "Seen" &&
-                    languages != "NotSeen" &&
-                    languages != "LastTimeWrong") { 
-                    vocabulary[i].WordList = tempWordlist;
+                if (entry.WordList != "Marked_-_-" &&
+                    entry.WordList != "Seen_-_-" &&
+                    entry.WordList != "NotSeen_-_-" &&
+                    entry.WordList != "LastTimeWrong_-_-") { 
+                    vocabulary[i].WordList = entry.WordList;
                     vocabulary[i].FirstLanguage = entry.FirstLanguage;
                     vocabulary[i].SecondLanguage = entry.SecondLanguage;
                 }
@@ -47,67 +44,7 @@ namespace VocabTrainer.Views {
             string json = JsonConvert.SerializeObject(vocabulary, Formatting.Indented);
             File.WriteAllText(entry.FilePath, json);
         }
-        public static (bool, string) CheckInput(string wordlist, string firstLanguageWord, string secondLanguageWord) {
-            int index = wordlist.IndexOf("(");
-            string fileName = wordlist.Substring(0, index - 1);
-            VocabularyEntry entry = new VocabularyEntry() { 
-                FilePath = $"{VocabularyEntry.FirstPartFilePath}{fileName}{VocabularyEntry.SecondPartFilePath}",
-                German = firstLanguageWord.Trim(),
-                English = secondLanguageWord.Trim(),
-            };
-
-            if (firstLanguageWord == "" || secondLanguageWord == "") {
-                return (false, $"Adding was not successful because one input box is empty");
-            } else {
-                List<VocabularyEntry> vocabulary = GetData(entry);
-                (bool isTrue, int index, int error, string word) returnedValues = alreadyThere(vocabulary, entry.German, entry.English);
-                if (!returnedValues.isTrue) {
-                    vocabulary.Add(entry);
-                    WriteData(entry, vocabulary);
-                    MoveWords("NotSeen", entry);
-                    return (true, $"'{firstLanguageWord}' and '{secondLanguageWord}' were successfully added");
-                } else {
-                    if (returnedValues.error == 1) {
-                        return (false, $"Adding was not successful because '{returnedValues.word}' already exists in the program");
-                    } else if (returnedValues.error == 2) {
-                        return (false, $"Adding was not successful because the entry '{firstLanguageWord}' and '{secondLanguageWord}' already exists in the program");
-                    } else if (returnedValues.error == 3) {
-                        return (false, $"Adding was not successful because '{returnedValues.word}' already exists in the program");
-                    } else {
-                        return (false, "");
-                    }
-                }
-            }
-        }
-        public static (bool, int, int, string) alreadyThere(List<VocabularyEntry> vocabulary, string firstLanguageWord, string secondLanguageWord) {
-            for (int i = 0; i < vocabulary.Count(); i++) {
-                if (vocabulary[i].German.ToLower() == firstLanguageWord.ToLower()) {
-                    return (true, i, 1, firstLanguageWord);
-                } else if (vocabulary[i].English.ToLower() == secondLanguageWord.ToLower()) {
-                    return (true, i, 1, secondLanguageWord);
-                } else if (vocabulary[i].German.ToLower().ToLower() == secondLanguageWord && vocabulary[i].English.ToLower() == firstLanguageWord) {
-                    return (true, i, 2, "");
-                } else if (vocabulary[i].German.ToLower().ToLower() == secondLanguageWord) {
-                    return (true, i, 3, secondLanguageWord);
-                } else if (vocabulary[i].English.ToLower() == firstLanguageWord) {
-                    return (true, i, 3, firstLanguageWord);
-                }
-            }
-            return (false, -1, -1, "");
-        }
-        public static List<string> CheckEmpty(List<VocabularyEntry> vocabulary) {
-            if (vocabulary.Count == 0) {
-                return new List<string> { "Es sind keine Wörter verfügbar", "There are no words available" };
-                ;
-            } else return new List<string> { string.Empty, string.Empty };
-        }
-        public static void MoveWords(string wordlist, VocabularyEntry words) {
-            VocabularyEntry entry = words;
-            entry.FilePath = $"{VocabularyEntry.FirstPartFilePath}{wordlist}{VocabularyEntry.SecondPartFilePath}";
-            List<VocabularyEntry> entries = GetData(entry);
-            entries.Add(entry);
-            WriteData(entry, entries);
-        }
+        
         public override bool Equals(object obj) {
             if (obj == null || GetType() != obj.GetType()) {
                 return false;
@@ -145,10 +82,6 @@ namespace VocabTrainer.Views {
             VocabularyEntry.WriteData(tempEntry, list);
         }
 
-        public static void PartyCorrectAnswer(VocabularyEntry entry) {
-            List<VocabularyEntry> entries = GetData(entry);
-            int index = entries.IndexOf(entry);
-        }
         public static bool CheckAnswer(VocabularyEntry entry, VocabularyEntry answer) {
             bool isCorrect = false;
             entry.FilePath = $"{FirstPartFilePath}{entry.WordList}{SecondPartFilePath}";
@@ -171,10 +104,6 @@ namespace VocabTrainer.Views {
             }
             WriteData(entry, entries);
             return isCorrect;
-        }
-        public static void FalseAnswer(VocabularyEntry entry) {
-            List<VocabularyEntry> entries = GetData(entry);
-            int index = entries.IndexOf(entry);
         }
     }
 }
