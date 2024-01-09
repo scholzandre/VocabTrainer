0976cb9de01be5390416edf936d16b0cfe41f1a3
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-01-09 16:00:28 +0100

    #update vm property

--- VocabTrainer/Views/AddView.xaml.cs
+++ VocabTrainer/Views/AddView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using VocabTrainer.ViewModels;
 
 namespace VocabTrainer.Views {
     public partial class AddView : UserControl {
@@ -8,6 +9,14 @@ namespace VocabTrainer.Views {
         private void Windows_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Windows_MouseDown(sender, e);
+        }
+
+        private void FirstWordTextBox_Loaded(object sender, System.Windows.RoutedEventArgs e) {
+            if (sender is TextBox textBox) {
+                if (DataContext is AddViewModel viewModel) {
+                    viewModel.FirstWordTextBox = textBox;                                               // store current TextBox, to focus it later on 
+                }
+            }
         }
     }
 }
