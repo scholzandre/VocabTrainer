3ae0f7a83246ba9c2f22b18d8cf5ccebe075916f
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-01-10 18:50:41 +0100

    #update vm textbox property

--- VocabTrainer/Views/AddWordlistView.xaml.cs
+++ VocabTrainer/Views/AddWordlistView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using VocabTrainer.ViewModels;
 
 namespace VocabTrainer.Views {
     public partial class AddWordlistView : UserControl {
@@ -9,6 +10,14 @@ namespace VocabTrainer.Views {
         private void Windows_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Windows_MouseDown(sender, e);
+        }
+
+        private void WordlistTextbox_Loaded(object sender, RoutedEventArgs e) {
+            if (sender is TextBox textBox) {
+                if (DataContext is AddWordlistViewModel viewModel) {
+                    viewModel.WordlistTextBox = textBox;                                               // store current TextBox, to focus it later on 
+                }
+            }
         }
     }
 }
